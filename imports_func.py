264f9e8398596165944635c1e1a519727a97ce4b
martiguesc <49061204+martiguesc@users.noreply.github.com>
2019-05-31 12:29:48 +0200

    Update imports_func.py

--- imports_func.py
+++ imports_func.py
@@ -3,4 +3,14 @@ import matplotlib.pyplot as plt
 import csv
 import os
 import pandas as pd
+import numpy as np
+from scipy.stats import shapiro
+from scipy.stats import levene
+import scipy.stats as stats
+from scipy import *
+import scipy
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import mean_absolute_error, accuracy_score
+from sklearn.cross_validation import train_test_split
 
